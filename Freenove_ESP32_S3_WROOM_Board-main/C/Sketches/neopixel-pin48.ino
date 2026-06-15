ea03153c82e9f16873dd34e51a2ca9184a5d30e4
SmartSpace <jaysys@gmail.com>
2026-06-15 20:53:25 +0900

    Update neopixel-pin48.ino

--- Freenove_ESP32_S3_WROOM_Board-main/C/Sketches/neopixel-pin48.ino
+++ Freenove_ESP32_S3_WROOM_Board-main/C/Sketches/neopixel-pin48.ino
@@ -1,1 +1,48 @@
 
+// Adafruit NeoPixel - pinmap 48번
+
+/*
+ * Project: ESP32-S3-N16R8 Onboard RGB LED Testing
+ * Created by: KES TECH (YouTube Channel)
+ * Description: Basic RGB color cycling for onboard WS2812B LED (GPIO 48)
+ */
+
+#include <Adafruit_NeoPixel.h>
+
+// Onboard RGB LED Configuration
+#define PIN        48   // ESP32-S3 onboard LED Pin
+#define NUMPIXELS   1   // Number of LEDs
+
+Adafruit_NeoPixel pixels(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);
+
+void setup() {
+  Serial.begin(115200);
+  Serial.println("--- Welcome to KES TECH ---");
+  Serial.println("Starting RGB LED Test...");
+  
+  pixels.begin(); 
+  pixels.setBrightness(50); // Brightness 0-255 (Keep it low to protect eyes)
+}
+
+void loop() {
+  // RED Color
+  Serial.println("Color: RED");
+  pixels.setPixelColor(0, pixels.Color(255, 0, 0)); 
+  pixels.show();
+  delay(1000);
+
+  // GREEN Color
+  Serial.println("Color: GREEN");
+  pixels.setPixelColor(0, pixels.Color(0, 255, 0)); 
+  pixels.show();
+  delay(1000);
+
+  // BLUE Color
+  Serial.println("Color: BLUE");
+  pixels.setPixelColor(0, pixels.Color(0, 0, 255)); 
+  pixels.show();
+  delay(1000);
+
+  // Channel Shoutout in Serial Monitor
+  Serial.println("Subscribe to KES TECH for more projects!");
+}
